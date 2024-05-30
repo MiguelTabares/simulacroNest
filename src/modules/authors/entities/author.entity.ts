ac90ae056a8b430691eba5c06a5831504acb03b0
MiguelTabares <silenceisbeautyofsoul@gmail.com>
2024-05-30 17:03:04 -0500

    commit2

--- src/modules/authors/entities/author.entity.ts
+++ src/modules/authors/entities/author.entity.ts
@@ -1,1 +1,35 @@
-export class Author {}
+import {
+    Column,
+    CreateDateColumn,
+    DeleteDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,  
+} from "typeorm";
+
+@Entity({ name: 'authors' })
+export class Author {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ name: 'name' })
+    name: string;
+
+    @Column()
+    lastname: string;
+
+    @Column()
+    birthDate: Date;
+
+    @Column()
+    nationality: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+    
+    @DeleteDateColumn({ nullable: true })
+    deletedAt?: Date;
+}
--- src/modules/books/entities/book.entity.ts
+++ src/modules/books/entities/book.entity.ts
@@ -1,1 +1,35 @@
-export class Book {}
+import {
+    Column,
+    CreateDateColumn,
+    DeleteDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,  
+} from "typeorm";
+
+@Entity({ name: 'books' })
+export class Author {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ name: 'title' })
+    title: string;
+
+    @Column()
+    lastname: string;
+
+    @Column()
+    birthDate: Date;
+
+    @Column()
+    nationality: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+    
+    @DeleteDateColumn({ nullable: true })
+    deletedAt?: Date;
+}
